b7583e08a351187dd90706b9bec91b9f67a2919a
amypyae <ampyaephyonaing@gmail.com>
2024-01-03 13:37:48 +0800

    Book Create

--- books/src/App.js
+++ books/src/App.js
@@ -1,7 +1,17 @@
-
+import { useState } from 'react';
+import BookCreate from './components/BookCreate';
 function App() {
+    const [books, setBooks] = useState([]);
+
+    const createBook = (title) => {
+        const updatedBooks = [...books, {id:Math.random(), title}];
+        setBooks(updatedBooks);
+    } 
+
     return (
-        <div>Welcome</div>
+        <div>
+            <BookCreate onCreate={ createBook }/>  
+        </div>
     );
 }
 
--- books/src/components/BookCreate.js
+++ books/src/components/BookCreate.js
@@ -1,7 +1,28 @@
-function BookCreate() {
+import { useState } from "react";
+
+function BookCreate({ onCreate }) {
+    const [title, setTitle] = useState('');
+
+    const handleSubmit = (event) => {
+        event.preventDefault();
+        onCreate(title);
+        setTitle('');
+    }
+
+    const handleChange = (event) => {
+        setTitle(event.target.value);
+    }
+
     return (
-        <div>Book Create</div>
-    )
+        <div className="book-create">
+            <h3>Add a Book</h3>
+            <form onSubmit={ handleSubmit }>
+                <label>Title</label>
+                <input className="input" value={ title } onChange={ handleChange }/>
+                <button className="button">Submit</button>
+            </form>
+        </div>
+    );
 }
 
 export default BookCreate;
