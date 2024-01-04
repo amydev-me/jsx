2d87c40a79a0ee637d94e7e7c444ff9a7250a68f
Amy <ampyaephyonaing@gmail.com>
2024-01-04 23:07:45 +0800

    Refactoring with Context

--- books/src/App.js
+++ books/src/App.js
@@ -1,11 +1,10 @@
-import { useState, useEffect } from 'react';
+import { useEffect, useContext } from 'react';
 import BookCreate from './components/BookCreate';
 import BookList from './components/BookList';
-import axios from 'axios';
+import BooksContext from './context/books'; 
 
 function App() {
-    const [books, setBooks] = useState([]);
-
+    const { fetchBooks } = useContext(BooksContext);
     /**
      * second argument is []      = Called after first render, never called again
      * second argument is -       = Called after first render, also called after every renender
@@ -13,61 +12,13 @@ function App() {
      */
     useEffect(() => {
         fetchBooks();
-    }, [])
-
-    const fetchBooks = async () => {
-        const { data } = await axios.get('http://localhost:3001/books');
-        setBooks(data);
-    }
-
-    const createBook = async (title) => {
-        const { data } = await axios.post('http://localhost:3001/books',{title});
-        
-        const updatedBooks = [
-            ...books, 
-            data
-        ];
-
-        setBooks(updatedBooks);
-    } 
-
-    const deleteBookById = async (id) => {
-        await axios.delete(`http://localhost:3001/books/${id}`);
-
-        const updatedBooks = books.filter((book) => {
-            return book.id !== id;
-        })
-
-        setBooks(updatedBooks);
-    }
-
-    const editBookById = async (id, newTitle) => {
-        const { data } = await axios.put(`http://localhost:3001/books/${id}`,{
-            title: newTitle
-        });
-       
-        const updatedBooks = books.map((book) => {
-            if(book.id === id){ 
-                return { ...book, ...data };
-            }
-
-            return book;
-        });
-
-        setBooks(updatedBooks);
-    }
+    }, []);
 
     return (
         <div className="app">
             <h1>Reading List</h1>
-            <BookList 
-                books={books} 
-                onDelete={ deleteBookById } 
-                onEdit={ editBookById }
-            />
-            <BookCreate 
-                onCreate={ createBook }
-            />  
+            <BookList  />
+            <BookCreate />  
         </div>
     );
 }
