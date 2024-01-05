a1684d55e64df6922e1d119b7149a0c02a73579a
Amy <ampyaephyonaing@gmail.com>
2024-01-05 23:06:57 +0800

    Button styling with tailwind

--- comps/src/App.js
+++ comps/src/App.js
@@ -1,6 +1,28 @@
+import Button from "./Button";
+
 function App() {
     return (
-        <div>Welcome</div>
+        <div>
+            <div>
+                <Button secondary outline  rounded>Click Me!</Button>
+            </div>
+
+            <div>
+                <Button danger  outline>Click!</Button>
+            </div>
+
+            <div>
+                <Button warning  outline>Click!</Button>
+            </div>
+
+            <div>
+                <Button secondary  outline>Click!</Button>
+            </div>
+
+            <div>
+                <Button primary  rounded>Click!</Button>
+            </div>
+        </div>
     );
 }
 
