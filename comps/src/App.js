e45a925aa06bc9330d4b49cf4fc93595dab74c63
Amy <ampyaephyonaing@gmail.com>
2024-01-07 00:33:29 +0800

    Create Custom Router

--- comps/src/App.js
+++ comps/src/App.js
@@ -1,9 +1,24 @@
+import Route from "./components/Route";
+import AccordionPage from './pages/AccordionPage';
 import DropDownPage from "./pages/DropDownPage";
+import Sidebar from "./components/Sidebar";
+import ButtonPage from "./pages/ButtonPage";
 
 function App() {
   return (
-    <div>
-      <DropDownPage />
+    <div className="container mx-auto grid grid-cols-6 gap-4 mt-4">
+       <Sidebar />
+      <div className="col-span-5">
+      <Route path="/buttons">
+          <ButtonPage />
+        </Route>
+        <Route path="/accordion">
+          <AccordionPage />
+        </Route>
+        <Route path="/">
+          <DropDownPage />
+        </Route>
+      </div>
     </div>
   );
 }
