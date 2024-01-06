149c3d6f7d056aa8e4b87b0e68c411d053a063f6
Amy <ampyaephyonaing@gmail.com>
2024-01-06 21:54:05 +0800

    Custom Dropdown with Panel

--- comps/src/components/DropDown.js
+++ comps/src/components/DropDown.js
@@ -1,6 +1,6 @@
 import { useState } from 'react';
 import { GoChevronDown } from 'react-icons/go';
-
+import Panel from './Panel';
 function DropDown({ options, value, onChange }) {
   const [ isOpen, setIsOpen ] = useState(false);
 
@@ -24,15 +24,17 @@ function DropDown({ options, value, onChange }) {
   
   return (
     <div className="w-48 relative">
-      <div 
-        className="flex justify-between items-center cursor-pointer border rounded p-3 shawdow bg-white w-full" 
-        onClick={ handleClick }>{value?.label || 'Select...'}
+      <Panel 
+        className="flex justify-between items-center cursor-pointer" 
+        onClick={ handleClick }
+      >
+        {value?.label || 'Select...'}
         <GoChevronDown className="text-lg"/>
-      </div>
+      </Panel>
       { isOpen && (
-        <div className="absolute top-full border rounded p-3 shadow bg-white w-full">
+        <Panel className="absolute top-full">
           { renderOptions }
-        </div> 
+        </Panel> 
       )}
     </div>
   );
