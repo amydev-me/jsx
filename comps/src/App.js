40debe7e296d156cb4cf550a873c913002cf5266
Amy <ampyaephyonaing@gmail.com>
2024-01-06 10:03:28 +0800

    button Styling using tailwind

--- comps/src/App.js
+++ comps/src/App.js
@@ -1,29 +1,39 @@
 import Button from "./Button";
+import { GoBell, GoDatabase } from 'react-icons/go';
 
 function App() {
     return (
+      <div>
         <div>
-            <div>
-                <Button secondary outline  rounded>Click Me!</Button>
-            </div>
-
-            <div>
-                <Button danger  outline>Click!</Button>
-            </div>
-
-            <div>
-                <Button warning  outline>Click!</Button>
-            </div>
-
-            <div>
-                <Button secondary  outline>Click!</Button>
-            </div>
-
-            <div>
-                <Button primary  rounded>Click!</Button>
-            </div>
+          <Button secondary outline rounded className="mb-5">
+            <GoBell />
+            Click me!!
+          </Button>
         </div>
+        <div>
+          <Button danger outline>
+            
+            Buy Now!
+          </Button>
+        </div>
+        <div>
+          <Button warning>
+            <GoDatabase />
+            See Deal!
+          </Button>
+        </div>
+        <div>
+          <Button secondary outline>
+            Hide Ads!
+          </Button>
+        </div>
+        <div>
+          <Button primary outline>
+            Something!
+          </Button>
+        </div>
+      </div>
     );
-}
+  }
 
 export default App;
