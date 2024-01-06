eb2375b2fadc23fce32d84d70a0085c31935e764
Amy <ampyaephyonaing@gmail.com>
2024-01-06 10:16:52 +0800

    Refactoring with Organization

--- comps/src/App.js
+++ comps/src/App.js
@@ -1,37 +1,7 @@
-import Button from "./Button";
-import { GoBell, GoDatabase } from 'react-icons/go';
 
 function App() {
     return (
       <div>
-        <div>
-          <Button secondary outline rounded className="mb-5">
-            <GoBell />
-            Click me!!
-          </Button>
-        </div>
-        <div>
-          <Button danger outline>
-            
-            Buy Now!
-          </Button>
-        </div>
-        <div>
-          <Button warning>
-            <GoDatabase />
-            See Deal!
-          </Button>
-        </div>
-        <div>
-          <Button secondary outline>
-            Hide Ads!
-          </Button>
-        </div>
-        <div>
-          <Button primary outline>
-            Something!
-          </Button>
-        </div>
       </div>
     );
   }
