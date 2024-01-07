d423550d2a55a6688096535b9ef13301bf072f70
Amy <ampyaephyonaing@gmail.com>
2024-01-07 14:26:13 +0800

    Create Reuseable table

--- comps/src/App.js
+++ comps/src/App.js
@@ -4,12 +4,15 @@ import DropDownPage from "./pages/DropDownPage";
 import Sidebar from "./components/Sidebar";
 import ButtonPage from "./pages/ButtonPage";
 import ModalPage from "./pages/ModalPage";
-
+import TablePage from './pages/TablePage';
 function App() {
   return (
     <div className="container mx-auto grid grid-cols-6 gap-4 mt-4">
        <Sidebar />
       <div className="col-span-5">
+        <Route path="/table">
+          <TablePage />
+        </Route>
         <Route path="/modal">
           <ModalPage />
         </Route>
