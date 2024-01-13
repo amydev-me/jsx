ae1d00ee56b88b70dc54874c2dc0cc98ac20b272
amy-uop <ampyaephyonaing@gmail.com>
2024-01-14 00:07:05 +0800

    Fetch users using thunks

--- media/src/components/UsersList.js
+++ media/src/components/UsersList.js
@@ -1,8 +1,30 @@
-
-
+import Panel from './Panel';
+import { useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { fetchUsers } from '../store';
 
 function UsersList() {
-  return 'Users List';
+  const dispatch = useDispatch();
+  const {isLoading, data, error} = useSelector((state) => {
+    return state.users;
+  })
+  useEffect(() => {
+    dispatch(fetchUsers());
+  }, [dispatch]);
+
+  if(isLoading) {
+    return <div>Loading...</div>
+  }
+
+  if(error){
+    return <div>Error fetching data...</div>
+  }
+
+  return (
+    <div>
+      {data.length}
+    </div>
+  );
 }
 
 export default UsersList;
