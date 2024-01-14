2c03db085dbeb4d7c6dfd623be2af7f18cee4ad7
amy-uop <ampyaephyonaing@gmail.com>
2024-01-14 15:21:46 +0800

    Creating Reuseable thunk hook

---
+++ media/src/store/thunks/createUser.js
@@ -0,0 +1,14 @@
+import { createAsyncThunk } from '@reduxjs/toolkit';
+import axios from 'axios';
+import { faker } from '@faker-js/faker';
+
+const addUser = createAsyncThunk('users/add', async(payload) => {
+  const { data } = await axios.post('http://localhost:3005/users', {
+    name: faker.person.fullName()
+  });
+
+  return data;
+});
+ 
+
+export { addUser };
