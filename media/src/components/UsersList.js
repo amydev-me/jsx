8720fcec54993f50f1d363dacaabf50130fd922f
amy-uop <ampyaephyonaing@gmail.com>
2024-01-14 15:32:30 +0800

    Better Skeleton display

--- media/src/components/UsersList.js
+++ media/src/components/UsersList.js
@@ -1,7 +1,7 @@
-import Button from './Button';
 import { useEffect } from 'react';
 import { useSelector } from 'react-redux';
 import { fetchUsers, addUser } from '../store';
+import Button from './Button';
 import Skeleton from './Skeleton';
 import useThunk from '../hooks/use-thunk';
 
@@ -22,39 +22,33 @@ function UsersList() {
     doCreateUser();
   }
 
+  let content;
+
   if(isLoadingUsers) {
-    return <Skeleton times={6} className="h-10 w-full" />;
-  }
-
-  if(loadingUsersError){
-    return <div>Error fetching data...</div>
-  }
-
-  const renderedUsers = data.map((user) => {
+    content = <Skeleton times={6} className="h-10 w-full" />;
+  }else if(loadingUsersError){
+    content = <div>Error fetching data...</div>
+  }else{
+    content = data.map((user) => {
     return (
-      <div key={user.id} className="mb-2 border rounded">
-        <div className="flex p-2 justify-between items-center cursor-pointer">
-          {user.name }
+        <div key={user.id} className="mb-2 border rounded">
+          <div className="flex p-2 justify-between items-center cursor-pointer">
+            {user.name }
+          </div>
         </div>
-      </div>
-    );
-  });
+      );
+    });
+  }
+ 
 
   return (
     <div>
-      <div className="flex flex-row justify-between m-3">
+      <div className="flex flex-row justify-between items-center m-3">
         <h1 className="m-2 text-xl">Users</h1>
-        {
-          isCreatingUser 
-            ? 'Creating User...'
-            : <Button onClick={handleUserAdd}>+ Add User</Button>
-
-        }
-        {
-          creatingUserError && 'Error creating user...'
-        }
+        <Button loading={isCreatingUser} onClick={handleUserAdd}>+ Add User</Button>
+        { creatingUserError && 'Error creating user...' }
       </div>
-      {renderedUsers}
+      {content}
     </div>
   );
 }
