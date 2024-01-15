adbf0f0c6a0ec84f5f5f0ff5809116452be55fb2
amy-uop <ampyaephyonaing@gmail.com>
2024-01-15 23:22:06 +0800

    Fetching Albums with RTK Query API

--- media/src/store/index.js
+++ media/src/store/index.js
@@ -1,12 +1,22 @@
-import {configureStore} from '@reduxjs/toolkit';
+import { configureStore } from '@reduxjs/toolkit';
+import { setupListeners } from '@reduxjs/toolkit/query';
 import { usersReducer } from './slices/usersSlice';
+import { albumsApi } from './apis/albumsApi';
 
 export const store = configureStore({
   reducer: {
-    users: usersReducer
+    users: usersReducer,
+    [albumsApi.reducerPath]: albumsApi.reducer // [albumsApi.reducerPath key of albums API = reducerPath
+  },
+  middleware: (getDefaultMiddleware) => {
+    return getDefaultMiddleware()
+      .concat(albumsApi.middleware);
   }
 });
+
+setupListeners(store.dispatch);
 
 export * from './thunks/fetchUsers';
 export * from './thunks/createUser';
 export * from './thunks/removeUser';
+export { useFetchAlbumsQuery } from './apis/albumsApi';
